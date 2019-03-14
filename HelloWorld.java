60ec1b5dd8fdf0bb61be523b48bbc13a018d1bf6
David Wang <DavidWang@aliyun.com>
2019-03-14 08:45:13 +0800

    David has modified the HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,11 @@
 public class HelloWorld {
-  public static void main(String[] args) {
-    System.out.println("Hello, this is java's programming world!");
-   }
-  }
-  
+	public static void main(String[] args) {
+		System.out.println("Hello, this is java's programming world!");
+	}
+}
+ 
+
+class User {
+	private int id;
+	private String name;
+} 
